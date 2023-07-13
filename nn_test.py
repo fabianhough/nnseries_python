caa2b73ba1d4a7292e06e2128a84676077d42e16
Fabian Hough <fabianhough@gmail.com>
2023-07-13 00:48:28 +0000

    Adding array ops and feed forward

--- nn_test.py
+++ nn_test.py
@@ -1,5 +1,6 @@
 
 from src import NeuralNetwork
+from src.neural import NeuralArray, dot_array
 
 
 
@@ -12,9 +13,20 @@ if __name__ == '__main__':
     print('WEIGHTS:')
     for w in nn.weights:
         print(w)
+        print(w.shape)
+        print(w.T)
+        print(w[0][0])
         print()
 
     print('BIAS:')
     for b in nn.bias:
         print(b)
+        print(b.shape)
         print()
+
+
+    print('\n\nTESTING')
+    sample_input = NeuralArray(3, src_array=[1,2,3])
+
+    sample_output = nn.feed_forward(sample_input)
+    print(sample_output)
--- src/neural.py
+++ src/neural.py
@@ -13,15 +13,52 @@ def build_array(dim1, dim2):
         new_array.append(new_row)
     return new_array
 
+def build_flat_array(dim):
+    return [random.random() * 2 - 1 for i in range(dim)]
+
+
+def dot_array(flat_array, dim_array):
+    assert flat_array.shape[0] == dim_array.shape[0]
+    result = []
+    for arr in dim_array.T:
+        result.append(sum([flat_array[i]*arr[i] for i in range(len(flat_array))]))
+
+    return NeuralArray(dim1=len(result), src_array=result)
+
+def add_array(flat_array1, flat_array2):
+    assert len(flat_array1) == len(flat_array2)
+    return NeuralArray(
+        dim1=len(flat_array1),
+        src_array=[flat_array1[i]+flat_array2[i] for i in range(len(flat_array1))]
+    )
+
+def subtract_array(flat_array1, flat_array2):
+    assert len(flat_array1) == len(flat_array2)
+    return NeuralArray(
+        dim1=len(flat_array1),
+        src_array=[flat_array1[i]-flat_array2[i] for i in range(len(flat_array1))]
+    )
+
 class NeuralArray():
-    def __init__(self, dim1, dim2):
+    def __init__(self, dim1: int, dim2: int=None, src_array: list=None):
         self.dim1 = dim1
         self.dim2 = dim2
-        self.array = build_array(dim1, dim2)
+        if src_array is None:
+            if dim2 is None:
+                self.array = build_flat_array(dim1)
+            else:
+                self.array = build_array(dim1, dim2)
+        else:
+            self.array = src_array
 
     @property
     def shape(self):
         return (self.dim1, self.dim2)
+
+    def __getitem__(self, idx):
+        if idx >= self.dim1:
+            raise IndexError(f'{idx} Larger Than {self.dim1}')
+        return self.array[idx]
 
     def __repr__(self):
         repr_str = '['
@@ -32,6 +69,31 @@ class NeuralArray():
         repr_str += ']'
         return repr_str
 
+    def __len__(self):
+        return len(self.array)
+
+    @property
+    def T(self):
+        assert not self.dim2 is None
+        T_array = []
+        for j in range(self.dim2):
+            T_row = []
+            for i in range(self.dim1):
+                T_row.append(self.array[i][j])
+            T_array.append(T_row)
+        return NeuralArray(self.dim2, self.dim1, T_array)
+
+    def apply_func(self, func):
+        if self.dim2 is None:
+            return NeuralArray(dim1=self.dim1, src_array=[func(a) for a in self.array])
+        else:
+            new_array = []
+            for i in range(self.dim1):
+                new_array.append([func(a) for a in self.array[i]])
+            return NeuralArray(dim1=self.dim1, dim2=self.dim2, src_array=new_array)
+
+
+
 ### END ARRAY ###
 
 
@@ -41,7 +103,9 @@ def relu(x):
     return max(0, x)
 
 def relu_deriv(x):
-    return 1 if x > 0
+    if x > 0:
+        return 1
+    return 0
 
 def sigmoid(x):
     return 1.0/(1.0 + math.exp(-x))
@@ -64,6 +128,8 @@ class NeuralNetwork():
 
         self.weights = []
         self.bias = []
+
+        self.layers = {}
     
     def initialize_network(self):
         net_dims = [self.input_size] + self.hidden_size_list + [self.output_size]
@@ -71,7 +137,26 @@ class NeuralNetwork():
         for i in range(len(net_dims)-1):
             layer_dims = (net_dims[i], net_dims[i+1])
             self.weights.append(NeuralArray(layer_dims[0], layer_dims[1]))
-            self.bias.append(NeuralArray(layer_dims[1], 1))
+            self.bias.append(NeuralArray(layer_dims[1]))
+
+            self.layers[i] = [1 for j in range(net_dims[i+1]+1)]
+
+    def feed_forward(self, input_array):
+        assert len(input_array) == self.input_size
+
+        self.layers[0] = input_array
+        for i in range(len(self.weights)):
+            result = add_array(dot_array(self.layers[i], self.weights[i]), self.bias[i])
+            if i < len(self.weights) - 1:
+                result = result.apply_func(relu)
+            else:
+                result = result.apply_func(sigmoid)
+
+            self.layers[i+1] = result
+
+        return self.layers[len(self.layers.keys())-1]
+
+        
 
 
 ### END NEURAL NETWORK
